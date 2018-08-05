21be36c26efe4df72d12e6ad1f1f93ab64316c5b
Manu New Deepin <tu@outlook.com.pe>
2018-08-05 08:14:56 -0500

    06.2 Admin/post SoftDeletesTable

--- app/Post.php
+++ app/Post.php
@@ -3,14 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
 
+    use SoftDeletes;
+
     protected $fillable = [
 
-        'title', 'contenido', 'category_id','featured'
+        'title', 'contenido', 'category_id','featured','slug'
     ];
+
+    protected $dates  = ['deleted_at'];
+
     // un post solo puede pertenecer a una sola categoria
     public function category()
     {
--- database/migrations/2018_07_30_224133_create_posts_table.php
+++ database/migrations/2018_07_30_224133_create_posts_table.php
@@ -17,10 +17,12 @@ class CreatePostsTable extends Migration
             $table->increments('id');
 
             $table->string('title');
+            $table->string('slug');
             $table->text('contenido');
             $table->integer('category_id');
             $table->string('featured');
 
+            $table->softDeletes();
             $table->timestamps();
         });
     }
