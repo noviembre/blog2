b119b75bdd2512cfc29db269c8dfad571393e1cb
Manu Mint Laptop <realgarcilaso@outlook.com>
2018-08-07 06:01:20 -0500

    12.1 Admin/profile - Relacion uno a uno - seeder profile
    empezamos a crear el perfil del usuario, comenzando por las relaciones. modificamos la tabla uuser y creamos un SEEDER para el PROFILE de USER.

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -9,12 +9,24 @@ class UsersTableSeeder extends Seeder
      *
      * @return void
      */
+
     public function run()
     {
-        App\User::create([
-            'name' => 'Manu',
-            'email' => 'a@ya.com',
-            'password' => bcrypt('123456')
+        $user = App\User::create([
+           'name' => 'Manu',
+           'email' => 'm@ya.com',
+           'password' => bcrypt('password'),
+            'admin'=> 1
+       ]);
+
+        # creando perfil para el admin
+        App\Profile::create([
+           'user_id' => $user->id,
+            'avatar'=> 'uploads/avatars/avatar_admin.png',
+            'about' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid consectetur consequuntur, ea eaque eius eos exercitationem inventore laboriosam laudantium magnam modi numquam quam quos rem rerum sequi veritatis voluptatem.',
+            'facebook'=> 'facebook.com',
+            'youtube' => 'youtube.com'
         ]);
     }
+
 }
